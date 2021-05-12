8245c9aa5527c69214b0b1ed591e83152f2e1604
Christina Delpone <cdelpone@gmail.com>
2021-05-12 15:19:27 -0600

    Initial commit

--- lib/classroom.rb
+++ lib/classroom.rb
@@ -1,2 +1,22 @@
 class Classroom
+  attr_reader :subject, :capacity, :students
+
+  def initialize(subject, capacity)
+    @subject = subject
+    @capacity = capacity
+    @students = []
+  end
+
+  def subject
+    @subject
+  end
+
+  def capacity
+    @capacity
+  end
+
+  def students
+    []
+  end
+
 end
